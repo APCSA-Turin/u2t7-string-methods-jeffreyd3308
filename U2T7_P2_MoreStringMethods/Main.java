07712911250c1b041fec5b487a9ea9b2ec5d6118
jeffreyd3308 <jeffreyd3308@nycstudents.net>
2024-10-08 16:07:17 +0000

    commit

--- U2T7_P2_MoreStringMethods/Main.java
+++ U2T7_P2_MoreStringMethods/Main.java
@@ -7,9 +7,11 @@ public class Main {
         int score = 0;
         String word;
         String altword;
+        int amountOfWords = 0;
 
         System.out.print("Choose a word: ");
         word = scan.next();
+        amountOfWords += 1;
         scan.nextLine();
 
         while (score < 50) {
@@ -19,29 +21,39 @@ public class Main {
 
             if ((altword.compareTo(word)) > 0) {
                 score += 2;
+                System.out.println("+2 because word is after alphabetically");
             } else if ((altword.compareTo(word)) < 0) {
                 score -= 5;
+                System.out.println("-5 because word is before alphabetically");
             } else {
                 score -= 10;
+                System.out.println("-10 because word is same alphabetically");
             }
             
             if (word.substring((word.length() - 2)).equals(altword.substring(0, 2))) {
                 score += 5;
+                System.out.println("+5 because first 2 letters of word contains last 2 letters of comparison word");
             }
 
             if (altword.indexOf(word.substring(0, 1)) != -1) {
                 score += 3;
+                System.out.println("+3 because word contains first letter");
             }
 
             if (altword.length() == word.length()) {
                 score += 1;
+                System.out.println("+1 because word is same in length");
             }
 
             word = altword;
+            amountOfWords += 1;
 
             System.out.println(score);
         }
 
+        System.out.println("Used " + amountOfWords + " words. Try again to get a better score");
+
         scan.close();
     }
 }
+
